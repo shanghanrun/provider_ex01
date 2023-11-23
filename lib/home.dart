37ad87961604f40e68e79ae6bb755585a9df0cbe
shanghanrun <120538041+shanghanrun@users.noreply.github.com>
2023-11-24 00:54:05 +0900

    botoom

--- lib/home.dart
+++ lib/home.dart
@@ -8,43 +8,56 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var counter = Provider.of<Counter>(context, listen: false);
+    // var counter = Provider.of<Counter>(context, listen: false);
     return Scaffold(
       appBar: AppBar(title: const Text('Provider Sample')),
-      body: const Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CountPage(),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.movie), label: 'Movie'),
+          BottomNavigationBarItem(icon: Icon(Icons.business), label: '회사'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: '학교'),
         ],
+        currentIndex: 0,
+        selectedItemColor: Colors.red,
+        onTap: (index) {},
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconButton(
-                iconSize: 50,
-                icon: const Icon(Icons.add_circle, color: Colors.red),
-                onPressed: () {
-                  counter.add();
-                },
-              ),
-              const SizedBox(width: 30, height: 30),
-              IconButton(
-                iconSize: 50,
-                icon: const Icon(Icons.remove_circle_outline_rounded,
-                    color: Colors.blue),
-                onPressed: () {
-                  counter.minus();
-                },
-              ),
-              const SizedBox(width: 30, height: 30),
-            ],
-          ),
-          const SizedBox(width: 30, height: 30),
-        ],
-      ),
+
+      //     const Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         CountPage(),
+      //       ],
+      //     ),
+      //     floatingActionButton: Column(
+      //       mainAxisAlignment: MainAxisAlignment.end,
+      //       children: [
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.end,
+      //           children: [
+      //             IconButton(
+      //               iconSize: 50,
+      //               icon: const Icon(Icons.add_circle, color: Colors.red),
+      //               onPressed: () {
+      //                 counter.add();
+      //               },
+      //             ),
+      //             const SizedBox(width: 30, height: 30),
+      //             IconButton(
+      //               iconSize: 50,
+      //               icon: const Icon(Icons.remove_circle_outline_rounded,
+      //                   color: Colors.blue),
+      //               onPressed: () {
+      //                 counter.minus();
+      //               },
+      //             ),
+      //             const SizedBox(width: 30, height: 30),
+      //           ],
+      //         ),
+      //         const SizedBox(width: 30, height: 30),
+      //       ],
+      //     ),
     );
   }
 }
